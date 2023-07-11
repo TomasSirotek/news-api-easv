a7ff0a26dbae133dd18f9d61e13e6690c3013e6a
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-07-11 14:41:42 +0200

    async with some remaining changes

--- Infrastructure/InfrastructureUtilityService.cs
+++ Infrastructure/InfrastructureUtilityService.cs
@@ -4,8 +4,19 @@ namespace Infrastructure;
 
 public static class InfrastructureUtilityService
 {
-    public static readonly string
-        ProperlyFormattedConnectionString = "Server=localhost;Database=postgres;Username=postgres;Password=postgres;Port=5432;Pooling=true;MaxPoolSize=3;";
+    // 1. Elephantsql.com
+   // private static readonly Uri Uri = new Uri(Environment.GetEnvironmentVariable("pgconn")!);
+    
+    // 2. Local docker-compose config
+    public const string ProperlyFormattedConnectionString = "Server=localhost;Database=postgres;Username=postgres;Password=postgres;Port=5432;Pooling=true;MaxPoolSize=3;";
+    
+    // public static readonly string ProperlyFormattedConnectionString =
+    // string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=3",
+    //     Uri.Host,
+    //     Uri.AbsolutePath.Trim('/'),
+    //     Uri.UserInfo.Split(':')[0],
+    //     Uri.UserInfo.Split(':')[1],
+    //     Uri.Port > 0 ? Uri.Port : 5432);
     
     public static void TestDataSource(NpgsqlDataSource dataSource)
     {
---
+++ api/Helpers/StatusCodeType.cs
@@ -0,0 +1,9 @@
+namespace api.Helpers;
+
+public enum StatusCode
+{
+    Success = 200,
+    NotFound = 404,
+    BadRequest = 400,
+    InternalServerError = 500
+}
